18dfe455c949fa3e965e2aedf64faa370bb43ac2
A01378360 <65176620+A01378360@users.noreply.github.com>
2022-03-24 14:12:11 -0600

    Update paint.py
    Modificación de comentario

--- paint.py
+++ paint.py
@@ -117,7 +117,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
-onkey(lambda: color('pink'), 'P') '''Se agrega un color como los anteriores, se define el nombre del color y la letra con la que será identificado y seleccionado''' "Brenda Vega Méndez"
+onkey(lambda: color('pink'), 'P') #Se agrega un color como los anteriores, se define el nombre del color y la letra con la que será identificado y seleccionado "Brenda Vega Méndez"
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
